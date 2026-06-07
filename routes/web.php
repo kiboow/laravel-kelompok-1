42030f40090c7402609d3bb2799c4d29b507286c
Saepul <umamsaepul703@gmail.com>
2026-06-07 11:38:35 +0700

    membuat halaman tentang kami

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
 
-// mengarahkan ke halaman utama (/) ke HomeController method index
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/tentang-kami', [AboutController::class, 'index'])->name('about');
