d7f67fa05210abf86d480d70655ab23b7e71afe5
Saepul <umamsaepul703@gmail.com>
2026-06-07 14:19:31 +0700

    update tampilan tentang kami dan navigasi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,20 +24,20 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
         <div class="container">
-            <a class="navbar-brand fw-bold text-info" href="#">{{ $namaAplikasi }}</a>
+            <a class="navbar-brand fw-bold text-info" href="{{ url('/') }}">{{ $namaAplikasi }}</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse id="navbarNav">
+            <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link active fw-semibold" href="#">Home</a>
+                        <a class="nav-link active fw-semibold" href="{{ url('/') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Fitur</a>
+                        <a class="nav-link" href="{{ url('/#fitur') }}">Fitur</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Tentang Kami</a>
+                        <a class="nav-link" href="{{ url('/tentang-kami') }}">Tentang Kami</a>
                     </li>
                 </ul>
             </div>
@@ -47,7 +47,7 @@
     <header class="hero-section text-center shadow-inner">
         <div class="container">
             <h1 class="display-4 fw-extrabold mb-3">{{ $namaAplikasi }}</h1>
-            <p class="lead mx-auto text-light-50" style="max-w: 700px;">{{ $deskripsi }}</p>
+            <p class="lead mx-auto text-light-50" style="max-width: 700px;">{{ $deskripsi }}</p>
             <div class="mt-4">
                 <a href="#fitur" class="btn btn-info btn-lg px-4 me-2 fw-bold text-white">Jelajahi Fitur</a>
                 <a href="#" class="btn btn-outline-light btn-lg px-4">Dokumentasi</a>
